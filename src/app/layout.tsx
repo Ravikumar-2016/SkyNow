6468e13fbc2f92b86328730634de611335c2ef8a
Ravikumar-2016 <ravikumargunti837@gmail.com>
2025-06-30 18:06:41 +0530

    corrected lint errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import { NetworkBanner } from "@/components/ui/network-banner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -52,6 +53,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <NetworkBanner></NetworkBanner>
          <Analytics />
       </body>
     </html>
